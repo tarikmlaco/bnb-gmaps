054f73facfa6f7f027823982ab2a84873b4aff4c
tarikmlaco <tarikmlaco@gmail.com>
2015-11-10 20:11:59 +0100

    first working combo

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+/* require('./main.css');
+var component = require('./component');
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
+
+*/
+
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+main();
+
+function main() {
+	const app = document.createElement('div');
+	document.body.appendChild(app);
+	
+	ReactDOM.render(<App />, app);
+}
