f2b956d0a8e64012c18027b221cd38675e8f146a
tarikmlaco <tarikmlaco@gmail.com>
2015-11-10 21:06:35 +0100

    changes etc.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,35 @@
+import uuid from 'node-uuid';
 import React from 'react';
 import Note from './Note.jsx';
 
-export default() => {
-	return <Note />;
+const notes = [
+	{
+		id: uuid.v4(),
+		task: 'Learn Webpack'
+	},
+	{
+		id: uuid.v4(),
+		task: 'Learn React'
+	},
+	{
+		id: uuid.v4(),
+		task: 'Do laundry'
+	}
+]
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<ul>{notes.map(this.renderNote)}</ul>
+			</div>
+		);
+	}
+	renderNote(note) {
+		return (
+			<li key={note.id}>
+				<Note task={note.task} />
+			</li>
+		);
+	}
 };
